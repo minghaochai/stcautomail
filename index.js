083db5bbb94ada1816d1d9f7eda3698c9246fd54
minghaochai <chaiminghao69@gmail.com>
2020-07-05 16:22:57 +0800

    updated express to integrate with azure

---
+++ index.js
@@ -0,0 +1,105 @@
+const cors = require("cors");
+const http = require('http');
+const express = require("express");
+const nodemailer = require("nodemailer");
+const bodyParser = require("body-parser");
+
+const details = require("./details.json");
+const countries = require("./country.json")
+
+const app = express();
+app.use(cors({ origin: "*" })); 
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+    res.send(
+      "<h1 style='text-align: center'>test nodemailer</h1>"
+    );
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port);
+
+app.get("/CountryList", (req, res) => {
+    console.log("in country")
+    res.send(
+        countries
+        );
+});
+  
+app.post("/emailRequest", (req, res) => {
+    console.log("in mail request")
+    console.log(req);
+    let user = req.body;
+    sendMail(user, info => {
+      res.send(info);
+    });
+})
+  
+app.post("/sendmail", (req, res) => {
+    console.log("in send mail")
+    let user = req.body;
+    sendMail(user, info => {
+      console.log(`The mail has been sent and the id is ${info.messageId}`);
+      res.send(info);
+    });
+});
+
+app.post("/test", (req, res) => {
+    console.log("test success")
+});
+
+app.get("/testmail", (req, res) => {
+    console.log("in test mail with get");
+    let user = { "test":"xd",
+                "requestorName":"test001",
+                "requestorMail":"haha@gmail.com",
+                "requestorContact":"012-3243",
+                "requestorAddress":"no 1 test street",
+                "requestorCountry":"Singapore",
+                "requestorCarModel":"Toyota",
+                "requestorFrame":"xc123",
+                "requestorRemarks":"hi i am testing"            
+            }
+    sendMail(user, info => {
+        res.send(info);
+    });
+});
+  
+async function sendMail(user, callback) {
+    console.log(user);
+    let currentDate =  new Date();
+    let mailSubject = '';
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      service: 'gmail',
+      secure: false,
+      port: 25,
+      auth: {
+        user: details.email,
+        pass: details.password
+      },
+      tls: {
+        rejectUnauthorized: false
+      }
+    });
+  
+    let mailOptions = {
+        from: '"Haomc" <haomcapptest@gmail.com',
+        //from: user.requestorMail,
+        to: 'haomcapptest@gmail.com',
+        subject:  mailSubject.concat('Enquiry from ', user.requestorMail, ' on ', currentDate),
+        text: 'Name: ' + user.requestorName + '\n' +
+                'Contact number: ' + user.requestorContact + '\n' +
+                'Address: ' + user.requestorAddress + '\n' +
+                'Country: ' + user.requestorCountry + '\n' +
+                'Car Model: ' + user.requestorCarModel + '\n' +
+                'Frame: ' + user.requestorFrame + '\n' +
+                'Remarks: ' + user.requestorRemarks + '\n'
+    };
+  
+    // send mail with defined transport object
+    let info = await transporter.sendMail(mailOptions);
+  
+    callback(info);
+}
